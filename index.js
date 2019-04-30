961b30f45e56aa8ddced1d53a3b31a768d003db8
Kevin Cavalcanti <k.w9cavalcanti@gmail.com>
2019-04-30 18:02:27 -0300

    Example of Parameters in URL

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const http = require("http");
 const express = require("express");
 const app = express();
 
-http
-  .createServer((req, res) => {
-    console.log(req);
-    res.end("Hello World");
-  })
-  .listen(3000);
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/nome/:name", (req, res) => {
+  res.send(`Bem-vindo ${req.params.name}`);
+});
+
+// Parâmetros na url
+
+// Parâmetros na
+
+app.listen(3333);
